9fd0a446dea56aebad3514e6f9f8f479306a42dd
fromandemo <roman.federico@gmail.com>
2015-04-09 23:20:33 +0100

    filtered data

--- plot1.r
+++ plot1.r
@@ -9,8 +9,13 @@ downloadData <- function() {
   
   if(!file.exists(zipFile)) { 
     download.file(url, zipFile, method = "curl") 
+    unzip(zipFile, exdir = "data") 
   }
-  unzip(zipFile, exdir = "data")   
 }
 
+downloadData()
+data<-read.csv("data/household_power_consumption.txt",sep =";")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data$Time <- strptime(data$Time,"%H:%M:%S")
 
+df2<-subset(data, data$Date=="2007-02-01" | data$Date=="2007-02-02")
